61ca2234942fb3d4fd928bdee2ce960d444cf82c
Nick Russell <n1russell@student.bridgew.edu>
2023-05-08 11:06:34 -0400

    added code to spawnitem.cs

--- Assets/Characters/SpawnItem.cs
+++ Assets/Characters/SpawnItem.cs
@@ -9,4 +9,17 @@ public class SpawnItem : MonoBehaviour
         Spawn();  
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // Spawn the item when a trigger is entered
+        if (collision.CompareTag("Player"))
+        {
+            Spawn();
+        }
+    }
+    private void Spawn()
+    {
+        Instantiate(itemPrefab, transform.position, Quaternion.identity);  // Instantiate the item prefab at the spawn point
+    }
+
 }
