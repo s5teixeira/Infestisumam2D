2a7084f7e79d0362ae095459aebb8ab14b97a6b9
s5teixeira <steixeira1555@gmail.com>
2023-05-08 10:30:17 -0400

    code for enemyspawner.cs

--- Assets/InfestisumamLogic/EnemySpawner.cs
+++ Assets/InfestisumamLogic/EnemySpawner.cs
@@ -23,4 +23,20 @@ public class EnemySpawner : MonoBehaviour
             timeSinceLastSpawn += Time.deltaTime;
         }
     }
+    private void SpawnEnemies()
+    {
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+            Instantiate(enemyPrefab, spawnPoint.position, Quaternion.identity, transform);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Player has entered the room start spawning enemies
+            enabled = true;
+        }
+    }
 }
